319b4a8ea2d24986087b2b76d424119591762342
Charles <charles.edwards@niwa.co.nz>
2018-08-26 23:25:56 +1200

    rebuild

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname)
 {
-    packageStartupMessage("bde version 1.0.0.1077 (2018-08-18 18:41:23)")
+    packageStartupMessage("bde version 1.0.0.1079 (2018-08-24 10:31:06)")
 }
