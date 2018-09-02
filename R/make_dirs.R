051f3c384868dbdab508e93cc871329dc5d23d7b
Charles <charles.edwards@niwa.co.nz>
2018-09-02 15:28:12 +1200

    update make_dirs.R

--- R/make_dirs.R
+++ R/make_dirs.R
@@ -1,15 +1,21 @@
 #'
 #' @title Make directories
 #' 
-#' @param model model
+#' @description Generates a list of directories of the form \code{file.path(root, model, lab, type)} and creates these directories if they are missing.
+#'
+#' @param model optional model name
 #' @param labels character vector of labels for each model run
-#' @param root root path to the directory
-#' @type type of model estimation being one of \code{MAP}, \code{VB}, \code{MCMC}
+#' @param root root path to the new directory or directories
+#' @type type of model estimation being one or more of \code{MAP}, \code{VB}, \code{MCMC}
+#' 
+#' @note by default an additional directory is also created of the form \code{file.path(root, model, lab, '.')} (i.e. without the \code{type}) specification for storage of model data inputs common to each estimation type.
 #' 
 #' @export
 make_dirs <- function(model, labels, root = "../../results", types = c("MAP", "VB", "MCMC")[3]) {
     
     paths <- list()
+	
+	types <- c(".", types)
     
     for (lab in labels) {
         
--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname)
 {
-    packageStartupMessage("bde version 1.0.0.1093 (2018-08-31 14:52:05)")
+    packageStartupMessage("bde version 1.0.0.1094 (2018-09-02 15:01:54)")
 }
