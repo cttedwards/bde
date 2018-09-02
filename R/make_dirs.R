b6550ebcc2603d8d4b7b024cbddee7d2a7fec845
Charles <charles.edwards@niwa.co.nz>
2018-09-02 14:48:29 +1200

    updated make_dirs.R

--- R/make_dirs.R
+++ R/make_dirs.R
@@ -7,17 +7,23 @@
 #' @type type of model estimation being one of \code{MAP}, \code{VB}, \code{MCMC}
 #' 
 #' @export
-make_dirs <- function(model, labels, root = "../../results", type = c("MAP", "VB", "MCMC")[3]) {
+make_dirs <- function(model, labels, root = "../../results", types = c("MAP", "VB", "MCMC")[3]) {
     
     paths <- list()
     
     for (lab in labels) {
-    
-        path <- file.path(root, model, tolower(lab), tolower(type), "/")
-        if (!dir.exists(path)) dir.create(path, recursive = TRUE)
         
-        paths[[lab]] <- path
+        paths[[lab]] <- list()
+        
+        for (type in types) {
     
+            if (!missing(model)) { path <- file.path(root, model, tolower(lab), tolower(type))
+            } else { path <- file.path(root, tolower(lab), tolower(type)) }
+            if (!dir.exists(path)) dir.create(path, recursive = TRUE)
+            
+            paths[[lab]][[type]] <- path
+        
+        }
     }
     
     return(paths)
