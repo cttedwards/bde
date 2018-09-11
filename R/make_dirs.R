52605b561fef29a7b3dde64c3cc4e83cd0407588
Charles <charles.edwards@niwa.co.nz>
2018-09-12 10:28:05 +1200

    updates to make_dirs.R

--- R/make_dirs.R
+++ R/make_dirs.R
@@ -1,37 +1,54 @@
 #'
 #' @title Make directories
 #' 
-#' @description Upon assignment, generates a list of directories of the form \code{file.path(root, model, lab, type)} and creates these directories if they are missing.
+#' @description Upon assignment, generates a list of directories of the form \code{file.path(root, model, label, type)} and creates these directories if they are missing.
 #'
-#' @param model optional model name
+#' @param model optional character string giving the model name
 #' @param labels character vector of labels for each model run
 #' @param root root path to the new directory or directories
-#' @type type of model estimation being one or more of \code{MAP}, \code{VB}, \code{MCMC}
+#' @param type type of model estimation being one or more of \code{MAP}, \code{VB}, \code{MCMC}
+#' @param clean logical indicating whether previously existent directories in the specified path locations should be deleted
 #' 
-#' @note by default an additional directory is also created of the form \code{file.path(root, model, lab, '.')} (i.e. without the \code{type}) specification for storage of model data inputs common to each estimation type.
+#' @note By default an additional directory is also created of the form \code{file.path(root, model, label, ".")} (i.e. without the \code{type}) specification for storage of model data inputs common to each estimation type.
 #' 
 #' @export
-make_dirs <- function(model, labels, root = "../../results", types = c("MAP", "VB", "MCMC")[3]) {
+make_dirs <- function(model, labels, root = "../../results", types = c("MAP", "VB", "MCMC")[3], clean = FALSE) {
     
     paths <- list()
 	
-	types <- c(".", types)
-    
     for (lab in labels) {
         
         paths[[lab]] <- list()
+        
+        if (!missing(model)) { paths[[lab]][["."]] <- file.path(root, model, tolower(lab))
+        } else { paths[[lab]][["."]] <- file.path(root, tolower(lab)) }
         
         for (type in types) {
     
             if (!missing(model)) { path <- file.path(root, model, tolower(lab), tolower(type))
             } else { path <- file.path(root, tolower(lab), tolower(type)) }
             
-            if (!dir.exists(path)) {
+            if (dir.exists(path)) {
+                
+                if (clean) {
+                    
+                    warning("deleted contents of ", path, " directory")
+                    unlink(path, recursive = TRUE) 
+                    
+                    message("creating clean ", path, " directory")
+                    dir.create(path, recursive = TRUE)
+                    
+                } else {
+                    
+                    message(path, " directory already exists, run with clean = TRUE?")    
+                }
+            } else {
+                
+                message("creating ", path, " directory")
                 dir.create(path, recursive = TRUE)
             }
             
             paths[[lab]][[type]] <- path
-        
         }
     }
     
