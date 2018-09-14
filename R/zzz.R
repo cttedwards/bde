90aa50912fae8a5449ebc4454d33f3279cd4e2de
Charles <charles.edwards@niwa.co.nz>
2018-09-15 11:18:30 +1200

    working on vignette

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname)
 {
-    packageStartupMessage("bde version 1.0.0.1135 (2018-09-12 10:44:23)")
+    packageStartupMessage("bde version 1.0.0.1137 (2018-09-13 18:45:28)")
 }
