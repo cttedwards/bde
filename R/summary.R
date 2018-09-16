e9e47b799d59ef0e0aec65e4e2a9f1affd4a0618
Charles <charles.edwards@niwa.co.nz>
2018-09-16 18:55:37 +1200

    fixed summary function for vectors

--- R/summary.R
+++ R/summary.R
@@ -1,12 +1,12 @@
 #'
-#' @title Summary function for maximum a posteriori estiamte
+#' @title Summary function for maximum a posteriori estimate
 #' 
 #' @description Takes as argument a return from a call to \code{\link{map}} and summarises the mean, sd and quantile values.
 #' 
 #' @param object return from a call to \code{\link{map}}
 #' @param pars   parameters to be summarised
-#' 
-#' @note This function will fail if the \code{draws} argument is not specified in the optimisation, i.e. \code{optimizing(..., draws = <n>)}, where \code{n} is a large number.
+#'
+#' @note This function will fail of the \code{draws} argument is not specified in the optimisation i.e. \code{optimizing(..., draws = <n>)}, where \code{<n>} is a large number.
 #' 
 #' @include map.R
 #' 
@@ -29,11 +29,44 @@
 #' @export
 "summary.map" <- function(object, pars) {
     
-    t(vapply(pars, function(x) c("mean" = object$estimate[[x]], 
-                                 "sd"   = object$sd[[x]],
-                                 object$quantiles[[x]][2],
-                                 object$quantiles[[x]][1],
-                                 object$quantiles[[x]][3]), numeric(5)))    
+    if (missing(pars)) {
+        
+        pars <- names(object$estimate)    
+    }
     
+    out <- list()
+    
+    if (is.null(object$sd)) {
+        
+        stop("Must specify 'draws' argument in call to optimizing to calculate standard errors and credibility intervals") 
+        
+    } else {
+    
+        for (x in pars) {
+            
+            n <- length(object$estimate[[x]])
+            
+            if (n > 1) {
+            
+                out[[x]] <- cbind("mean"  = object$estimate[[x]], 
+                                  "sd"    = object$sd[[x]],
+                                  "2.5%"  = object$quantiles[[x]][2,],
+                                  "50%"   = object$quantiles[[x]][1,],
+                                  "97.5%" = object$quantiles[[x]][3,])
+                
+                rownames(out[[x]]) <- paste0(x, "[", 1:n, "]")
+                
+            } else {
+                
+                out[[x]] <- cbind("mean"  = object$estimate[[x]], 
+                                  "sd"    = object$sd[[x]],
+                                  "2.5%"  = object$quantiles[[x]][2],
+                                  "50%"   = object$quantiles[[x]][1],
+                                  "97.5%" = object$quantiles[[x]][3])
+                
+            }
+        }
+    }
+    return(out)
 }
 
