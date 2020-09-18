b0328903c1fa3e13bfbb8aee53c3d4cf260bef35
Charles <cescapecs@gmail.com>
2020-09-18 14:46:42 +1200

    rebuild

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname)
 {
-    packageStartupMessage("bde version 1.0.0.1158 (2019-02-13 11:51:36)")
+    packageStartupMessage("bde version 1.0.0.1166 (2020-09-18 14:40:34)")
 }
